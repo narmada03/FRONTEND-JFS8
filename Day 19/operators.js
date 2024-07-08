71e73773b86c1bd67e4b29c1862b0289b255a1da
narmada03 <narmadagandla@gmail.com>
2024-07-08 16:04:17 +0530

    Add files via upload

---
+++ Day 19/operators.js
@@ -0,0 +1,95 @@
+console.log("Opetators in Js")
+//Arithemetic Operators
+/*
+let a = 4;
+let b = 5;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+*/
+//Assignment Operators
+/*var a = 5;
+console.log(a);
+a = a*2;
+console.log(a);
+a *=2;
+console.log(a);
+var a = 5;
+console.log(a);
+a = a+2;
+console.log(a);
+a +=2;
+console.log(a);
+*/
+/*console.log("Comparision Operators");
+//Equals to 
+var a = 10;
+var b ="10";
+console.log(a==b);
+//Equals to
+var a = 10;
+var b ="10";
+console.log(a===b);
+//not Equals to
+var a = 10;
+var b ="20";
+console.log(a!=b);
+
+var a = 10;
+var b ="20";
+console.log(a!==b);
+var a = 10;
+var b = 20;
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+*/
+//logical operators
+/*console.log("logical operator");
+var age = 30;
+var voter = true;
+console.log(age>=18);
+console.log(voter == true);
+console.log((age>=18) && (voter == true))
+console.log((age>=18) || (voter == true))
+console.log((age>=18) );
+
+//unary operator
+var x = 5;
+console.log(+x);
+var y = 5;
+console.log(-y);
+
+//ternary operator
+console.log("ternary operator");
+var age = 28;
+var result = (age>=18)  ? "can vote" :" cannot vote"
+console.log("result");
+*/
+
+console.log("Increment and decrement operators");
+var a1 =100;
+console.log(a1++);
+var a2 = 100;
+console.log(++a2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

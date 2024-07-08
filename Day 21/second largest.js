660bd53abd1797bb0b8d7c85e4dfcc4977f6ebdd
narmada03 <narmadagandla@gmail.com>
2024-07-08 16:06:43 +0530

    Add files via upload

---
+++ Day 21/second largest.js
@@ -0,0 +1,22 @@
+var a=10;
+var b=20;
+var c=30;
+if(a>b && a>c){
+    a=0
+}
+else if(b>a && a>c){
+   b=o
+}
+else{
+  c=0
+}
+if(a>b && a>c){
+    console.log(a);
+}
+else if(b>a && a>c){
+    console.log(b);
+}
+else{
+    console.log(c);
+}
+    
---
+++ Day 21/statement.js
@@ -0,0 +1,5 @@
+var x=100;
+var y=20;
+var y=y/2;
+var x=x-y;
+console.log(x);

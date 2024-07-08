b50001bc6742d654bdb2286718875142163ae651
narmada03 <narmadagandla@gmail.com>
2024-07-08 16:03:44 +0530

    Add files via upload

---
+++ Day 18/hello.js
@@ -0,0 +1,5 @@
+console.log("I am Hello.js file")
+var x = null
+console.log(x)
+var y ;
+console.log(y);
---
+++ Day 18/sample.js
@@ -0,0 +1,8 @@
+//Var variable
+var x = 10;
+console.log(x);
+x = 100;
+console.log(x);
+var x = 200;
+x = 200;
+console.log(x);

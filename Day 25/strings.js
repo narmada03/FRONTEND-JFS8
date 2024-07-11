9063f59887c98ae546ccb6c444eb7cade5232801
narmada03 <narmadagandla@gmail.com>
2024-07-11 21:57:06 +0530

    Add files via upload

---
+++ Day 25/strings.js
@@ -0,0 +1,21 @@
+console.log("Strings");
+//creation of strings
+var x="narmada";
+console.log(x);
+
+//finding lenth
+var a=x.length;
+console.log(a);
+
+//access elements
+console.log(x[4]);
+//concatenation
+var v1="narmada";
+var v2="gandla";
+var v3=v1+" "+v2+"!";
+console.log(v3);
+
+var x1='narmada';
+var x2='gandla';
+var x3=x1.concat(" ", x2 ,"!");
+console.log(x3);
